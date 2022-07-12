af7b2f6c28f8c23472dd5fee500510534d5df5e8
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-12 16:42:03 +0530

    Add files via upload

---
+++ file_import_os.py
@@ -0,0 +1,14 @@
+''' certain Linux file handling commands
+1. pwd- present working directory
+2. ls - list of files '''
+
+
+# import os
+# print(os.getcwd())
+
+import os 
+os.mkdir('python_batch')                                                                    # will create a folder directly
+# os.rmdir('python_batch')                                                                  # to delete a folder directly
+# os.remove('python_file_7.txt')                                                            # to remove the file directly
+# print(os.path.exists('python_file_5.txt'))                                                  # True
+# print(os.path.exists('python_file_9.txt'))                                                    # False
